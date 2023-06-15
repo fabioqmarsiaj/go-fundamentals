d44b491d8242a63400a8200107a2d9113add6b3f
fabioqmarsiaj <fabiobackend@gmail.com>
2023-06-15 08:37:01 -0300

    Packages and go mod

---
+++ 1 - packages/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Function with caps lock on first letter is public.
+func Write() {
+	fmt.Println("Writing from auxiliar package.")
+	write2()
+}
---
+++ 1 - packages/auxiliar/auxiliar2.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func write2() {
+	fmt.Println("Writing by function write2.")
+}
