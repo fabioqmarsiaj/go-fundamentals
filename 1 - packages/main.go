01551e0c0ae2e71f444e50b0151e09c7aa417fed
fabioqmarsiaj <fabiobackend@gmail.com>
2023-06-15 22:37:22 -0300

    External packages

--- 1 - packages/main.go
+++ 1 - packages/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Main function!")
 	auxiliar.Write()
+
+	erro := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(erro)
 }
